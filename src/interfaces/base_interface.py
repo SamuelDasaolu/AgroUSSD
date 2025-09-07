62121484bdfaa08e50343f8d4c88ff47c5b8981f
your name <ogorpaul877@gmail.com>
2025-09-07 13:22:15 +0100

    first_push

---
+++ src/interfaces/base_interface.py
@@ -0,0 +1,24 @@
+# def main_menu():
+#  menu = input("Select the number of any of the following\n1. Farmer Menu\n2. Buyer Menu\n3. Exit").strip()
+#  if menu == "1":
+#    farmer_choice = input("Are you a farmer: Yes/No").strip().lower()
+#    if farmer_choice != "yes":
+#      return("If you're not a farmer, select buyer or exit")
+#     #  return menu
+
+# def farmer_login():
+#   print("Register Farmer. \nName, \nPhone, \nLocation, \nFarm Size, /" \
+#   "\nPrimary Crops, \nSet Pin")
+
+
+       
+# while True:
+#     code = input("Enter AgroUSSD Code (*556#): ").strip()
+#     if code != "*556#":
+#      print("Invalid Code")
+#     else:
+#        print("Access Granted")
+#        main_menu()
+
+
+
